546f3673ecd913739d130a06d9ed02e7db503046
ehdgnsl01 <gnsl505@naver.com>
2025-01-21 20:25:36 +0900

    check

--- src/main/java/com/example/bangyo/controller/HomeController.java
+++ src/main/java/com/example/bangyo/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("인증 d객체: " + authentication);
+        System.out.println("인증 객체: " + authentication);
         System.out.println("인증된 사용자 이름: " + authentication.getName());
         if (authentication != null) {
             model.addAttribute("username", authentication.getName()); // 사용자 이름 전달
